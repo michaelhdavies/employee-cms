41cd6699f8a31121d43d2b7e37b5691afe918eb7
Michael Henry Davies <michael_methodtheatre.com>
2025-03-19 18:57:43 -0600

    working application

--- Develop/src/db/schema.sql
+++ Develop/src/db/schema.sql
@@ -6,31 +6,31 @@ CREATE DATABASE employee_cms;
 \c employee_cms;
 
 -- Create department table --
-CREATE TABLE department (
+CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 -- Create role table --
-CREATE TABLE role (
+CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    dept_id INTEGER,
+    FOREIGN KEY (dept_id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 -- Create employee table --
-CREATE TABLE employee (
+CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES roles(id)
     ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES employees(id)
     ON DELETE SET NULL
 );
